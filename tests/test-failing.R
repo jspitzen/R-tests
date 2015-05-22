f3ada7f21c195997f2b78d852d11f68b7583b523
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2015-05-22 17:07:15 +0200

    More tests

--- tests/test-failing.R
+++ tests/test-failing.R
@@ -6,3 +6,13 @@ test_that("6 does not equal 5", {
 test_that("5 does not equal sort of close to 5", {
   expect_that(5, equals(5 + 1e-6))
 })
+
+test_that("one failing expectation fails the test", {
+  expect_equals(5, 6)
+  expect_equals(4, 4)
+})
+
+test_that("both expectations failing fails the test",{
+  expect_equals(5, 6)
+  expect_equals(4, 4)
+})
