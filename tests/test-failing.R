1f1f96d69d34fe26b188db3d9885873c6856b186
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2015-05-22 17:03:18 +0200

    remove quotes from test name, thats just mean

--- tests/test-failing.R
+++ tests/test-failing.R
@@ -3,6 +3,6 @@ test_that("6 does not equal 5", {
   expect_that(6, equals(5))
 })
 
-test_that("5 does not equal 'sort of close' to 5", {
+test_that("5 does not equal sort of close to 5", {
   expect_that(5, equals(5 + 1e-6))
 })
