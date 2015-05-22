ef6e34a93ce9d3c395914d9b880772d2c9ce923a
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2015-05-22 16:11:43 +0200

    #1 Added file for failing test

--- tests/test-aaa-without-context.R
+++ tests/test-aaa-without-context.R
@@ -1,7 +1,3 @@
 test_that("5 euqals 5 without context", {
   expect_that(5, equals(5))
 })
-
-test_that("failing test outside context", {
-  expect_that(6, equals(5))
-})
--- tests/test-equals.R
+++ tests/test-equals.R
@@ -3,3 +3,8 @@ context("Equals expectation")
 test_that("5 euqals 5", {
   expect_that(5, equals(5))
 })
+
+test_that("5 equals very close to 5", {
+  expect_that(5, equals(5 + 1e-7))
+})
+
