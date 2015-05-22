022792d1f4dfeaadedd1699df6d56b404ad93c21
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2015-05-22 17:08:29 +0200

    typo fix

--- tests/test-failing.R
+++ tests/test-failing.R
@@ -8,11 +8,11 @@ test_that("5 does not equal sort of close to 5", {
 })
 
 test_that("one failing expectation fails the test", {
-  expect_equals(5, 6)
-  expect_equals(4, 4)
+  expect_equal(5, 6)
+  expect_equal(4, 4)
 })
 
 test_that("both expectations failing fails the test",{
-  expect_equals(5, 6)
-  expect_equals(4, 4)
+  expect_equal(5, 6)
+  expect_equal(4, 4)
 })
